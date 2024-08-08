9b2f4660e1562fc2a779f7c8b9a3a4d209b48c0a
VasiliyHaroshka <horvasiliy@mail.ru>
2024-08-08 17:55:42 +0300

    change database to postgres

--- education/education/config.py
+++ education/education/config.py
@@ -5,3 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DATA_BASE = os.environ.get("DATA_BASE")
+DB_NAME = os.environ.get("DB_NAME")
+USER_NAME = os.environ.get("USER_NAME")
+USER_PASSWORD = os.environ.get("USER_PASSWORD")
+HOST = os.environ.get("HOST")
--- education/education/settings.py
+++ education/education/settings.py
@@ -12,7 +12,14 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-from .config import SECRET_KEY
+from .config import (
+    SECRET_KEY,
+    DATA_BASE,
+    DB_NAME,
+    USER_NAME,
+    USER_PASSWORD,
+    HOST,
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +46,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "courses.apps.CoursesConfig",
 ]
 
 MIDDLEWARE = [
@@ -77,8 +86,11 @@ WSGI_APPLICATION = "education.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": f"django.db.backends.{DATA_BASE}",
+        "NAME": DB_NAME,
+        "USER": USER_NAME,
+        "PASSWORD": USER_PASSWORD,
+        "HOST": HOST,
     }
 }
 
