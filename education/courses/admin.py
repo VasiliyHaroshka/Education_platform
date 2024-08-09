f2718abce2aa50c538cb8fa2960928cfe699321c
VasiliyHaroshka <horvasiliy@mail.ru>
2024-08-09 15:36:00 +0300

    register Subject model

--- education/courses/admin.py
+++ education/courses/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Subject
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug")
+    list_display_links = ("title",)
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ("title",)
+    list_editable = ("title",)
+    save_on_top = True
