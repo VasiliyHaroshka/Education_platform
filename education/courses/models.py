e0e16ec3c23d5737430e47d21cbac84a2c6962c5
VasiliyHaroshka <horvasiliy@mail.ru>
2024-08-09 15:35:53 +0300

    create Subject model

--- education/courses/models.py
+++ education/courses/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    """
+    This model describes subject
+    """
+    title = models.CharField(
+        "Название",
+        max_length=250,
+    )
+    slug = models.SlugField(
+        "Слаг",
+        max_length=255,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ("title",)
+        verbose_name = "Предмет"
+        verbose_name_plural = "Предметы"
