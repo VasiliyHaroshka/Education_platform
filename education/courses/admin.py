92d4ad4fe999ff5d43c2626c354a5f3176a72290
VasiliyHaroshka <horvasiliy@mail.ru>
2024-08-09 15:54:15 +0300

    register Course model

--- education/courses/admin.py
+++ education/courses/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 
-from .models import Subject
+from .models import Subject, Course
 
 
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
+    """Register Subject model to admin panel"""
     list_display = ("title", "slug")
     list_display_links = ("title",)
     prepopulated_fields = {"slug": ("title",)}
     search_fields = ("title",)
+    list_editable = ("title",)
+    save_on_top = True
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    """Register Course model to admin panel"""
+    list_display = ("title", "description", "created_at")
+    list_display_links = ("title",)
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ("title", "description")
     list_editable = ("title",)
     save_on_top = True
