ac2a1b57ad8194e261fa79971483dc4a95f3588f
VasiliyHaroshka <horvasiliy@mail.ru>
2024-08-09 15:54:05 +0300

    crate model Course

--- education/courses/models.py
+++ education/courses/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -22,3 +23,39 @@ class Subject(models.Model):
         ordering = ("title",)
         verbose_name = "Предмет"
         verbose_name_plural = "Предметы"
+
+
+class Course(models.Model):
+    """
+    This model describes course
+    """
+    title = models.CharField(
+        "Название",
+        max_length=250,
+    )
+    slug = models.SlugField(
+        "Слаг",
+        max_length=250,
+        unique=True,
+    )
+    description = models.TextField(
+        "Описание",
+    )
+    created_at = models.DateTimeField(
+        "Дата создания",
+        auto_now_add=True,
+    )
+    author = models.ForeignKey(
+        User,
+        related_name="course_created",
+        on_delete=models.CASCADE,
+        verbose_name="Автор",
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ("-created_at",)
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
