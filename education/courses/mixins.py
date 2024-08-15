9dc5e9c741ae0ee7f84bb1219ad93f645e3d83d2
VasiliyHaroshka <horvasiliy@mail.ru>
2024-08-15 13:47:29 +0300

    add OwnerCourseMixin

--- education/courses/mixins.py
+++ education/courses/mixins.py
@@ -7,6 +7,7 @@ class OwnerMixin:
     """
     Mixin return queryset only from recent user
     """
+
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(author=self.request.user)
@@ -16,9 +17,16 @@ class OwnerEditMixin:
     """
     Mixin return edit form only for certain user
     """
+
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
 
-
+class OwnerCourseMixin(OwnerMixin):
+    """
+    Gives model, fields and success_url
+    """
+    model = Course
+    fields = ("title", "slug", "subject", "description")
+    success_url = reverse_lazy('manage_course_list')
