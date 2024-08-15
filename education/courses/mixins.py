9da64f6b8417787e68d8c5ad8c3c1bd728794f5b
VasiliyHaroshka <horvasiliy@mail.ru>
2024-08-15 13:33:21 +0300

    add OwnerEditMixin

--- education/courses/mixins.py
+++ education/courses/mixins.py
@@ -4,9 +4,21 @@ from .models import Course
 
 
 class OwnerMixin:
+    """
+    Mixin return queryset only from recent user
+    """
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(author=self.request.user)
+
+
+class OwnerEditMixin:
+    """
+    Mixin return edit form only for certain user
+    """
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 
