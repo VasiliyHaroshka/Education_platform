159ae077afcc7faa9c897bc42b66de896c88c5c0
VasiliyHaroshka <horvasiliy@mail.ru>
2024-08-18 18:04:47 +0300

    add permission_required to CRUD views courses app

--- education/courses/views.py
+++ education/courses/views.py
@@ -11,6 +11,7 @@ class CourseManageListView(ListView):
     """
     model = Course
     template_name = "courses/manage/course/list.html"
+    permission_required = "courses.view_course"
 
     def get_queryset(self):
         """
@@ -21,12 +22,13 @@ class CourseManageListView(ListView):
 
 
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
-    ...
+    permission_required = "courses.add_course"
 
 
 class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-    ...
+    permission_required = "courses.change_course"
 
 
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = "courses/manage/course/delete.html"
+    permission_required = "courses.delete_course"
