89ad5e1cae5247946bdfce3d4d829b0181baa557
VasiliyHaroshka <horvasiliy@mail.ru>
2024-08-18 18:31:09 +0300

    fix bugs

--- education/courses/urls.py
+++ education/courses/urls.py
@@ -10,6 +10,6 @@ from .views import (
 urlpatterns = [
     path("mine/", CourseManageListView.as_view(), name="manage_course_list"),
     path("create/", CourseCreateView.as_view(), name="create_course"),
-    path("<pk:int>/change/", CourseUpdateView.as_view(), name="change_course"),
-    path("<pk:int>/delete/", CourseDeleteView.as_view(), name="delete_course"),
+    path("<int:pk>/change/", CourseUpdateView.as_view(), name="change_course"),
+    path("<int:pk>/delete/", CourseDeleteView.as_view(), name="delete_course"),
 ]
--- education/education/urls.py
+++ education/education/urls.py
@@ -18,13 +18,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth import views as django_auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
 urlpatterns = [
-    path("accounts/login/", django_auth_views.LoginView.as_view(), name="login"),
-    path("accounts/logout/", django_auth_views.LogoutView.as_view(), name="logout"),
-    path("course/", include("courses.url")),
+    path("accounts/login/", LoginView.as_view(), name="login"),
+    path("accounts/logout/", LogoutView.as_view(), name="logout"),
+    path("course/", include("courses.urls")),
     path("admin/", admin.site.urls),
 ]
 
