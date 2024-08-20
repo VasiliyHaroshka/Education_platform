74182ce1f3a0d7c4f1a2d6c07c5f0f4c01b829d4
VasiliyHaroshka <horvasiliy@mail.ru>
2024-08-20 19:34:42 +0300

    add CourseAndModuleUpdateView

--- education/courses/views.py
+++ education/courses/views.py
@@ -1,6 +1,9 @@
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.list import ListView
 
+from .forms import ModuleFormSet
 from .mixins import OwnerCourseEditMixin, OwnerCourseMixin
 from .models import Course
 
@@ -32,3 +35,36 @@ class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = "courses/manage/course/delete.html"
     permission_required = "courses.delete_course"
+
+
+class CourseAndModuleUpdateView(TemplateResponseMixin, View):
+    """
+    This class manage addition, updating and deleting of certain course modules
+    """
+    template_name = "courses/manage/formset.html"
+    course = None
+
+    def get_formset(self, data=None):
+        """Get form ModuleFormSet and fill in it with data"""
+        return ModuleFormSet(instance=self.course, data=data)
+
+    def dispatch(self, request, id):
+        """
+        Send request to methods get or post.
+        Get model Course with recent user.
+        Save parameter "course" for other methods
+        """
+        self.course = get_object_or_404(Course, id=id, author=request.user)
+        return super().dispatch(request, id)
+
+    def get(self, request, *args, **kwargs):
+        """Return ana empty form and template with certain context"""
+        formset = self.get_formset()
+        return self.render_to_response({"course": self.course, "formset": formset})
+
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect("manage_course_list")
+        return self.render_to_response({"course": self.course, "formset": formset})
